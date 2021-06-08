f692840b5b544017df45e6b5a140a5c2c1b784ca
Nelson Amador <rod6214@gmail.com>
2021-06-08 02:39:43 -0300

    Added new react dom

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,28 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     output: {
-        path: path.resolve(__dirname, 'dist', 'js'),
-        filename: '[id].[contenthash].js'
-    }
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[id].[contenthash].js'
+    },
+
+    module: {
+        rules:[
+            {
+                test: /(\.js|\.jsx)$/,
+                exclude: /node_module/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            filename: 'index.html'
+        }),
+        new CleanWebpackPlugin()
+    ]
 }
